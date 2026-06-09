99c5031714314f1c748d90222751ac7e7533058d
SIDHARTH T <sidhartht900@gmail.com>
2026-06-09 17:52:16 +0530

    feat: add reverse and isPalindrome functions to dsa1.js

--- dsa1.js
+++ dsa1.js
@@ -17,6 +17,17 @@ function countDigits(n) {
     return 1 + countDigits(Math.floor(n / 10))
 }
 
-console.log(countDigits(123456))
+function reverse(str, i = str.length - 1) {
+    if (i < 0) return '';
 
+    return str[i] + reverse(str, i - 1);
+}
+
+function isPalindrome(str, start = 0, end = str.length - 1) {
+    if (start >= end) return true;
+
+    if (str[start] !== str[end]) return false;
+
+    return isPalindrome(str, start + 1, end - 1);
+}
 
