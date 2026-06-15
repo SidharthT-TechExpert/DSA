1d1775e8543436f7412a04f7bf1e6804553ba6af
SIDHARTH T <sidhartht900@gmail.com>
2026-06-15 17:54:17 +0530

    feat: add SelectionSort and InsertionSort functions to sort.js

--- DSA2/sort.js
+++ DSA2/sort.js
@@ -13,5 +13,40 @@ function bubbleSort(ar) {
     return ar;
 }
 
-console.log(bubbleSort(arr));
-console.log(arr)
+//Selection   Sort 
+function SelectionSort(ar) {
+    let n = ar.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (ar[j] < ar[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [ar[i], ar[minIndex]] = [ar[minIndex], ar[i]];
+        }
+    }
+    return ar;
+}
+
+//Insertion Sort
+function InsertionSort(ar) {
+    let n = ar.length;
+    for (let i = 1; i < n; i++) {
+        let key = ar[i];
+        let j = i - 1;
+        while (j >= 0 && ar[j] > key) {
+            console.log(ar[j], key)
+            ar[j + 1] = ar[j];
+            j--;
+        }
+        ar[j + 1] = key;
+    }
+    return ar;
+}
+
+console.log("Original array:", arr);
+// console.log(bubbleSort(arr));
+// console.log(SelectionSort(arr));
+console.log(InsertionSort(arr));
