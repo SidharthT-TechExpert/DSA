ea12da3b61adee5887702d52bd59bf4c079e8fac
SIDHARTH T <sidhartht900@gmail.com>
2026-06-16 20:41:28 +0530

    feat: implement merge sort algorithm in sort.js and add testSort.js file

--- DSA2/sort.js
+++ DSA2/sort.js
@@ -46,7 +46,41 @@ function InsertionSort(ar) {
     return ar;
 }
 
+//Merge Sort
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+
+    return merge(left,right);
+}
+
+function merge(left,right){
+    console.log(left, right)
+    let result = [];
+    let i = 0, j = 0;
+
+    // 5 2
+    while(i < left.length && j < right.length){
+        if(left[i] <= right[j]){
+            result.push(left[i]);
+            i ++;
+        }else{
+            result.push(right[j]);
+            j ++;
+        }
+    }
+
+    return result.concat(left.slice(i)).concat(right.slice(j));
+
+}
+
+
+
 console.log("Original array:", arr);
+console.log(mergeSort(arr))
 // console.log(bubbleSort(arr));
 // console.log(SelectionSort(arr));
-console.log(InsertionSort(arr));
+// console.log(InsertionSort(arr));
